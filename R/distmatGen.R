f002e7b63216615e64d95f4dbfe3bb549cb118c6
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-08-10 16:03:28 +0200

    Complete documentation
    and homogenize style

--- R/distmatGen.R
+++ R/distmatGen.R
@@ -1,60 +1,71 @@
-#'@name distmatGen
-#'@title Generate a cost-based distance matrix among SpatialPoints
-#'@export
-#'@import gdistance
-#'@details
-#'@param pts A SpatialPointsDataFrame with a defined projection
-#'@param costsurf cost surface Raster
-#'@param ret specify whether to return distances between obs-obs ("o"), obs-loc ("l"), or both ("b")
-#'@details If a cost surface raster is not supplied a uniform (cost 1) grid will be generated to match to bounding box of pts
-#'@example
-#'data(noise)
-#'r<-raster(nrows=40,ncols=40,resolution=1)
-#'r<-setExtent(r,extent(obs),keepres=T)
-#'aggfac<-5
-#'costsurf<-rasterize(malilla,r,background=aggfac,field=rep(10000,length(polygons(malilla))))
-#'costsurf<-reclassify(costsurf,c(aggfac+1,Inf,NA))
-#'costsurf<-aggregate(costsurf,aggfac,expand=T,fun=max,na.rm=T)
+#' Compute cost-based distances
 #'
+#' Generate a cost-based distance matrix among observations and/or between
+#' observations and prediction locations.
 #'
+#' Use the centroids of the cost surface raster cells as prediction locations.
 #'
-#'res<-distmatGen(obs,costsurf,ret="o")
-#'plot(dd.distmat[,51],res[,51],ylim=c(0,600),xlim=c(0,600))
-#'2
+#' If a cost surface raster is not supplied a uniform (cost 1) grid will be
+#' generated to match to bounding box of pts
 #'
+#' @export
+#' @import gdistance
+#' @param pts A SpatialPointsDataFrame with a defined projection
+#' @param costsurf cost surface Raster
+#' @param ret specify whether to return distances between obs-obs ("o"), obs-loc
+#'   ("l"), or both ("b")
+#' @param directions. See \code{\link[gdistance]{transition}}.
+#' @examples
+#'  data(noise)
+#'  r <- raster(nrows=40,ncols=40,resolution=1)
+#'  r <- setExtent(r,extent(obs),keepres=T)
+#'  aggfac <- 5
+#'  costsurf <- rasterize(malilla,r,background=aggfac,field=rep(10000,length(polygons(malilla))))
+#'  costsurf <- reclassify(costsurf,c(aggfac+1,Inf,NA))
+#'  costsurf <- aggregate(costsurf,aggfac,expand=T,fun=max,na.rm=T)
+#'
+#'  res <- distmatGen(obs,costsurf,ret="o")
+#'  plot(dd.distmat[,51],res[,51],ylim=c(0,600),xlim=c(0,600))
 distmatGen <- function(pts, costsurf, ret, directions = 16) {
 
-
-  #pts<-obs
+  #pts <- obs
   #ret="b"
-  r<-costsurf
 
-  if(!extent(r)>=extent(pts)){
-    xn<-floor(extent(pts)[1]-1)
-    xx<-ceiling(extent(pts)[2]+1)
-    yn<-floor(extent(pts)[3]-1)
-    yx<-ceiling(extent(pts)[4]+1)
-    ext<-unlist(lapply(list(xn,xx,yn,yx),function(x) round(x,0)))
-    r<-extend(r,extent(ext))
+  if (inherits(pts, 'data.frame')) {
+    ## cast to SpatialPoints
+    pts <- SpatialPoints(pts)
+  }
+
+  ## Rename cost surface
+  r <- costsurf
+
+  ## Extend surface if needed
+  if (!extent(r) >= extent(pts)) {
+    xn <- floor(extent(pts)[1]-1)
+    xx <- ceiling(extent(pts)[2]+1)
+    yn <- floor(extent(pts)[3]-1)
+    yx <- ceiling(extent(pts)[4]+1)
+    ext <- unlist(lapply(list(xn, xx, yn, yx),function(x) round(x, 0)))
+    r <- extend(r, extent(ext))
   }
 
   ## Transition matrix
   tr <- transition(r, geoR_trf, directions)
 
   if(ret=="b"|ret=="o"){
-  oret<-diag(0,nrow=nrow(pts))
-  oret.c<-matrix(costDistance(tr,coordinates(pts)))
-  oret[lower.tri(oret)]<-oret.c
-  oret<-oret+t(oret)
+  oret <- diag(0,nrow=nrow(pts))
+  oret.c <- matrix(costDistance(tr,coordinates(pts)))
+  oret[lower.tri(oret)] <- oret.c
+  oret <- oret+t(oret)
   }
 
   if(ret=="b"|ret=="l"){
-    fromcoords<-coordinates(pts)
-    #lret<-diag(0,nrow=nrow(tocoords))
-    lret.c<-matrix(costDistance(tr,fromcoords,tocoords),nrow=nrow(fromcoords))#CANNOT ALLOCATE ENOUGH MEMORY
-    #lret[lower.tri(lret)]<-lret.c
-    lret<-lret.c
+    fromcoords <- coordinates(pts)
     tocoords <- xyFromCell(tr,which(values(r)==res(r)))
+    #lret <- diag(0,nrow=nrow(tocoords))
+    lret.c <- matrix(costDistance(tr,fromcoords,tocoords),nrow=nrow(fromcoords))#CANNOT ALLOCATE ENOUGH MEMORY
+    #lret[lower.tri(lret)] <- lret.c
+    lret <- lret.c
     }
 
   if(ret=="b"){
